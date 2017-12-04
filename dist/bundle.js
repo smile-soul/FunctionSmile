3670b47eb04e57374feb33d0f19abf91cca61258
smile <517578601@qq.com>
2017-12-04 21:53:54 +0800

    feat: 增加rollup构建

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,3 +1,21 @@
+function __$styleInject(css, returnValue) {
+  if (typeof document === 'undefined') {
+    return returnValue;
+  }
+  css = css || '';
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  style.type = 'text/css';
+  head.appendChild(style);
+  
+  if (style.styleSheet){
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+  return returnValue;
+}
+
 var a = 2;
 console.log(a);
 //# sourceMappingURL=bundle.js.map
--- rollup/rollup.config.dev.js
+++ rollup/rollup.config.dev.js
@@ -3,6 +3,10 @@ import babel from 'rollup-plugin-babel';
 import json from 'rollup-plugin-json';
 import rollupTypescript from 'rollup-plugin-typescript';
 import commonjs from 'rollup-plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+import postcssModules from 'postcss-modules';
+import Visualizer from 'rollup-plugin-visualizer';
+import alias from 'rollup-plugin-alias';
 
 const devconfig = {
     input: './src/main.ts',
@@ -12,24 +16,44 @@ const devconfig = {
         name: 'lib',
         sourcemap: true
     },
-    plugins:[
+    plugins: [
         json(),
         rollupTypescript(),
         commonjs(),
+        Visualizer({
+            sourcemap: true
+          }),
+        alias({
+        somelibrary: './mylocallibrary'
+        }),
+        postcss({
+            plugins: [
+                // cssnext(),
+                // yourPostcssPlugin()
+                postcssModules({
+                    getJSON(id, exportTokens) {
+                        cssExportMap[id] = exportTokens;
+                    }
+                })
+            ],
+            sourceMap: true, // default value
+            //extract: false, // default value
+            extensions: ['.css', '.less']  // default value
+        }),
         resolve({
             // 将自定义选项传递给解析插件
             customResolveOptions: {
-              moduleDirectory: 'node_modules'
+                moduleDirectory: 'node_modules'
             }
-          }),
-          babel({
+        }),
+        babel({
             exclude: 'node_modules/**' // 只编译我们的源代码
-          }),
+        }),
     ],
     globals: {
         lodash: 'lodash'
-      },
-          // 指出应将哪些模块视为外部模块
+    },
+    // 指出应将哪些模块视为外部模块
     external: ['lodash']
 }
 
